237dbf1e8db03e17400ad27ad6f8b1c5737ac6ca
ashitadadlani <dadlani.a@husky.neu.edu>
2015-04-14 16:28:41 -0700

    Week7

---
+++ CS 5610/experiments/week7/004.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module("StarApp", ['ngRoute' , 'ui.bootstrap']);
+
+app.directive('rate', function () {
+    return {
+        restrict: 'A',
+        template: '<ul class="rating">' +
+        '<li ng-repeat="star in stars" ng-class="star">' +
+        '\u2605' +
+        '</li>' +
+        '</ul>',
+        scope: {
+            ratingValue: '=',
+            max: '='
+        },
+        link: function (scope, elem, attrs) {
+            scope.stars = [];
+            for (var i = 0; i < scope.max; i++) {
+                scope.stars.push({
+                    filled: i < scope.ratingValue
+                });
+            }
+        }
+    }
+});
+
+
+app.controller("MainCtrl", function ($scope, $http) {
+    $scope.rate1 = '1';
+    $scope.rate2 = '3';
+    $scope.rate3= '5';
+})
